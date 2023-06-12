10cc160da2f0bdafcdaefdd3fb988e7c376f64d1
chauhuuphat <chauhuuphat237@gmail.com>
2023-06-12 11:15:37 +0700

    Create Terraform Workspace

--- output.tf
+++ output.tf
@@ -1,39 +1,39 @@
-output "instance_1_id" {
-  description = "The ID of the example instance"
-  value       = try(aws_instance.instance_1.id)
-}
+# output "instance_1_id" {
+#   description = "The ID of the example instance"
+#   value       = try(aws_instance.instance_1.id)
+# }
 
-output "instance_2_id" {
-  description = "The ID of the example instance"
-  value       = try(aws_instance.instance_2.id)
-}
+# output "instance_2_id" {
+#   description = "The ID of the example instance"
+#   value       = try(aws_instance.instance_2.id)
+# }
 
-output "website_bucket_1_arn" {
-  description = "ARN of the bucket"
-  value       = module.website_s3_bucket.arn
-}
+# output "website_bucket_1_arn" {
+#   description = "ARN of the bucket"
+#   value       = module.website_s3_bucket.arn
+# }
 
-output "website_bucket_1_name" {
-  description = "Name (ID) of the bucket"
-  value       = module.website_s3_bucket.name
-}
+# output "website_bucket_1_name" {
+#   description = "Name (ID) of the bucket"
+#   value       = module.website_s3_bucket.name
+# }
 
-output "website_bucket_1_domain" {
-  description = "Domain of the bucket"
-  value       = module.website_s3_bucket.domain
-}
+# output "website_bucket_1_domain" {
+#   description = "Domain of the bucket"
+#   value       = module.website_s3_bucket.domain
+# }
 
-output "website_bucket_2_arn" {
-  description = "ARN of the bucket"
-  value       = module.website_s3_bucket_2.arn
-}
+# output "website_bucket_2_arn" {
+#   description = "ARN of the bucket"
+#   value       = module.website_s3_bucket_2.arn
+# }
 
-output "website_bucket_2_name" {
-  description = "Name (ID) of the bucket"
-  value       = module.website_s3_bucket_2.name
-}
+# output "website_bucket_2_name" {
+#   description = "Name (ID) of the bucket"
+#   value       = module.website_s3_bucket_2.name
+# }
 
-output "website_bucket_2_domain" {
-  description = "Domain of the bucket"
-  value       = module.website_s3_bucket_2.domain
-}
+# output "website_bucket_2_domain" {
+#   description = "Domain of the bucket"
+#   value       = module.website_s3_bucket_2.domain
+# }
