525274097879c3945c4a23fc49be67392c972e4a
chauhuuphat <chauhuuphat237@gmail.com>
2023-06-13 22:41:38 +0700

    lastest update

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+locals {
+  instance_types = {
+    dev = "t2.mirco"
+    uat = "t2.small"
+  }
+}
+
 resource "aws_key_pair" "terraform_best_practices_demo" {
   key_name   = "${terraform.workspace}-terraform-best-practices-demo-key"
   public_key = file("./.ssh/id_rsa.pub")
@@ -5,7 +12,7 @@ resource "aws_key_pair" "terraform_best_practices_demo" {
 
 resource "aws_instance" "instance_1" {
   ami           = var.instance_1_ami
-  instance_type = var.instance_1_type
+  instance_type = local.instance_types[terraform.workspace]
   tags = {
     Name = "${terraform.workspace}-${var.instance_1_name}"
   }
@@ -14,7 +21,7 @@ resource "aws_instance" "instance_1" {
 
 resource "aws_instance" "instance_2" {
   ami           = var.instance_2_ami
-  instance_type = var.instance_2_type
+  instance_type = local.instance_types[terraform.workspace]
   tags = {
     Name = "${terraform.workspace}-${var.instance_2_name}"
   }
